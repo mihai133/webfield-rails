678cbaa97af42136053432cb565da685c925f4b1
mihai133 <mihai.cngm@gmail.com>
2024-08-09 16:38:56 +0300

    Postcss config

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
-    equire('tailwindcss'),
+    require('tailwindcss'),
     require('autoprefixer'),
     require('postcss-preset-env')({
       autoprefixer: {
